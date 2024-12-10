4b86b53923ffbfb1bd7b60b6ce4e7f5eef343337
neoki <ot07neoki@gmail.com>
2024-12-10 23:06:34 +0900

    feat: setup command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use clap::Command;
+
 fn main() {
+    Command::new("depdog")
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("A tool to check the versions of project dependencies")
+        .get_matches();
+
     println!("Hello, world!");
 }
